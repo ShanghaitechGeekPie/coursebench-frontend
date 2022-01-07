fe048dc666ef1247e012c4f4ac6176afebb7c85a
wenxuanjun <wendster@163.com>
2022-01-07 20:19:39 +0800

    Fix: Path

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+import router from '@/router/index'
 
 Vue.config.productionTip = false
 
