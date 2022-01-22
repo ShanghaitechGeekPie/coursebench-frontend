2fa822fe3512aca84bbb60195c0c9ed70ceda925
yanglinshu <nicolasvandarkholme@outlook.com>
2022-01-22 23:25:26 +0800

    New: Comment card of User page

--- src/components/users/comment/Comment.vue
+++ src/components/users/comment/Comment.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-container class="px-0 py-sm-3 py-0">
+  <v-container class="py-sm-3 py-0">
     <v-row>
-      <v-col cols="12" md="3" order-md="last" class="pl-sm-0 pr-sm-3 pr-3">
+      <v-col cols="12" md="3" order-md="last" class="px-sm-0 px-0 pt-6 pt-sm-3">
         <v-info-card></v-info-card>
       </v-col>
-      <v-col cols="12" md="9" order-md="first" class="pr-sm-3">
+      <v-col cols="12" md="9" order-md="first" class="pl-sm-0 pr-sm-3 pl-0 pr-0 pt-sm-3 pt-0">
         <v-comt-card
           v-for="(comt, index) in usercomt.comment"
           :key="index"
