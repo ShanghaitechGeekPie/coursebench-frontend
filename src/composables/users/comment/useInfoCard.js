17d7a546cc7a23605b49f7c77797a411d40f9132
yanglinshu <nicolasvandarkholme@outlook.com>
2022-01-24 00:23:54 +0800

    New: Line limit and icons in the User page

--- src/composables/users/comment/useInfoCard.js
+++ src/composables/users/comment/useInfoCard.js
@@ -1,11 +1,15 @@
 import { inject, reactive } from "@vue/composition-api"
+import { mdiThumbUp, mdiCommentCheck } from "@mdi/js"
 
 export default () => {
 
     const comtinfo = inject('comtinfo')
 
     const statics = reactive({
-        
+        icons: {
+            mdiThumbUp, 
+            mdiCommentCheck
+        }
     })
 
     const status = reactive({
