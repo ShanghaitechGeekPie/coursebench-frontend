8d571c17f38620ae1fc47a6ca844eb170f7aea8d
Clarivy <zqc.sc.hn@gmail.com>
2022-01-31 00:29:55 +0800

    Add comment part

--- src/views/CourseDetails.vue
+++ src/views/CourseDetails.vue
@@ -3,21 +3,41 @@
     <div class="upper-wrp d-flex justify-center align-end">
       <DetailCard class="mt-3 mt-sm-15" />
     </div>
-    <div class="review-wrp">
-      <v-tabs centered>
-        <v-tab>All Groups</v-tab>
+    <div>
+      <v-tabs centered v-model="currentTab">
+        <v-tab v-for='(item, index) in items' :key="index">{{item.name}}</v-tab>
       </v-tabs>
+      <v-tabs-items v-model="currentTab"  class="transparent d-flex align-center justify-center">
+        <v-tab-item v-for="(item, index) in items" :key="index">
+          <CommentBoard :currentTab="items[currentTab]"></CommentBoard>
+        </v-tab-item>
+      </v-tabs-items>
     </div>
   </div>
 </template>
 
 <script>
 import DetailCard from '@/components/courses/DetailCard.vue'
+import CommentBoard from "@/components/courses/CommentBoard";
 
 export default {
-  components: { DetailCard },
+  components: {
+    DetailCard,
+    CommentBoard,
+  },
   data() {
     return {
+      currentTab: 0,
+      items: [
+        { name: '全部评价'},
+        { name: '高盛华' },
+        { name: '梁骏睿' },
+        { name: '娄鑫' },
+        { name: '刘闯' },
+        { name: '野兽先辈' },
+        { name: '林弘扬' },
+        { name: '关云长' },
+      ]
     }
   }
 }
