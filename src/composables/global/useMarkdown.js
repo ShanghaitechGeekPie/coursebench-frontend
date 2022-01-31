40554a17f9fe60810763ea31572fc4ee4efa7306
wenxuanjun <wendster@163.com>
2022-01-31 17:09:03 +0800

    Fix: Usage of the Markdown

--- src/composables/global/useMarkdown.js
+++ src/composables/global/useMarkdown.js
@@ -1,10 +1,8 @@
 import MarkdownIt from 'markdown-it'
 import prism from 'markdown-it-prism'
-import '@/styles/prism.css'
-import '@/styles/prism.css'
 
 export default (resultText) => {
-    const parser = new MarkdownIt()
-    parser.use(prism)
-    return parser.render(resultText)
+  const parser = new MarkdownIt()
+  parser.use(prism)
+  return parser.render(resultText)
 }
