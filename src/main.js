dfeed0eb814440ad526a9be649a015f3c70685a2
wenxuanjun <wendster@163.com>
2022-01-31 11:32:43 +0800

    Fix: Dependences version
    New: Import VueQuery

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import vuetify from '@/plugins/vuetify'
 import router from '@/router/index'
 import VueCompositionAPI from '@vue/composition-api'
 import NProgress from 'nprogress'
+import { VueQueryPlugin } from "vue-query"
 import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionAPI)
+Vue.use(VueQueryPlugin)
 
 router.beforeEach((to, from, next) => {
   NProgress.start()
