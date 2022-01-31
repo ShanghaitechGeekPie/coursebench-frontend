934099d9d8da998328a76c9bce444d7d1a48c39f
Clarivy <zqc.sc.hn@gmail.com>
2022-01-31 10:43:17 +0800

    Localize useComment.js and useCommentCard.js for courses

--- src/components/courses/CommentBoard.vue
+++ src/components/courses/CommentBoard.vue
@@ -1,19 +1,11 @@
 <template>
-  <v-container class="py-sm-3 py-0">
-    <v-row>
-      <v-col cols="12" md="3" order-md="last" class="px-sm-0 px-0 pt-6 pt-sm-3">
-        <StatisticCard/>
-      </v-col>
-      <v-col cols="12" md="9" order-md="first" class="pl-sm-0 pr-sm-3 pl-0 pr-0 pt-sm-3 pt-0">
-        <CommentBox v-for="(comment, index) in commentText" :key="index" :comment="comment" />
-      </v-col>
-    </v-row>
+  <v-container class="" style="background-color: white">
+    <CommentBox v-for="(comment, index) in commentText" :key="index" :comment="comment" />
   </v-container>
 </template>
 <script>
 import useUser from '@/composables/users/useUser'
-import useComment from "@/composables/users/comment/useComment";
-import StatisticCard from '@/components/users/comment/StatisticCard'
+import useComment from "@/composables/courses/comment/useComment";
 import CommentBox from '@/components/courses/CommentBox'
 
 export default {
@@ -22,6 +14,6 @@ export default {
     const { commentText, statics, status } = useComment();
     return { commentText, statics, status };
   },
-  components: { StatisticCard, CommentBox }
+  components: { CommentBox }
 };
 </script>
