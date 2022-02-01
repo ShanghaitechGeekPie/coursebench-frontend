5607b7f2bde96272c1f65ac20d36c343f2dc1b25
Clarivy <zqc.sc.hn@gmail.com>
2022-02-01 20:03:21 +0800

    Improve the outlook for desktop and add markdown support

--- src/components/courses/CommentBox.vue
+++ src/components/courses/CommentBox.vue
@@ -10,7 +10,7 @@
           ></v-img>
         </v-avatar>
         <div class="mx-2 text-body-2">
-          {{comment.is_anonymous ? "匿名用户" : comment.user_nickname}}
+          {{ comment.is_anonymous ? "匿名用户" : comment.user_nickname }}
         </div>
         <v-rating
             class="d-flex"
@@ -23,14 +23,41 @@
             dense
         ></v-rating>
         <v-col class="text-end text-body-2" style="color: gray">
-          {{comment.time}}
+          {{ comment.time }}
         </v-col>
       </v-row>
       <v-row class="mt-3">
         <v-card elevation="0">
-          <v-card-title class="font-weight-black text-body-1" >{{comment.title}}</v-card-title>
+          <v-card-title class="font-weight-black text-body-1">{{ comment.title }}</v-card-title>
           <v-card-text>
-            {{comment.comment}}
+            <div v-html="useMarkdown(comment.comment)">
+            </div>
+            <v-subheader>
+              <v-row>
+                <v-col class="pl-1">
+                  <div class="d-flex">
+                    <v-btn
+                        icon
+                        :color="status.liked ? 'light-blue' : 'gray'"
+                        x-small
+                    >
+                      <v-icon class="mr-1">{{statics.icons.mdiThumbUp}}</v-icon>
+                    </v-btn>
+                    <div class="font-weight-medium" :class="status.liked ? 'light-blue--text' : 'grey--text'">
+                      {{status.likedNum}}
+                    </div>
+                  </div>
+                </v-col>
+                <v-col
+                    v-for="(name, index) in statics.scoreNames" :key="index"
+                >
+                  <div class="d-flex flex-row text-no-wrap">
+                    <div class="font-weight-bold text-body-2 px-3">{{name}}</div>
+                    <div>{{ comment.score[index].toFixed(1) }}</div>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-subheader>
           </v-card-text>
         </v-card>
       </v-row>
@@ -39,12 +66,14 @@
   </v-container>
 </template>
 <script>
-import useCommentCard from "@/composables/courses/comment/useCommentCard";
+import useCommentBox from "@/composables/courses/comment/useCommentBox";
+import useMarkdown from '@/composables/global/useMarkdown'
+import colors from "vuetify/lib/util/colors"
 
 export default {
   setup() {
-    const { userProfile, status } = useCommentCard();
-    return { userProfile, status };
+    const {userProfile, statics, status} = useCommentBox();
+    return {userProfile, statics, status, useMarkdown, colors};
   },
   props: {
     comment: Object,
