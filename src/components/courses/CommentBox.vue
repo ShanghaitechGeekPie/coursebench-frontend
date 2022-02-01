1148f998fd5fd7e0bdafd295d8f7dbdc3110ae36
Clarivy <zqc.sc.hn@gmail.com>
2022-02-01 20:43:54 +0800

    Improve the outlook for multi-platform

--- src/components/courses/CommentBox.vue
+++ src/components/courses/CommentBox.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container>
-    <v-card class="my-5 pl-5" elevation="0" max-width="800px" width="100vw">
-      <v-row align="center" style="width: 100%;">
-        <v-avatar color="pink" size="38">
+  <v-container class="py-1">
+    <v-card class="my-5 pl-5 pt-0" elevation="0" max-width="800px" width="100vw">
+      <v-row class="pl-3" align="center" style="width: 100%;">
+        <v-avatar color="gray" size="38">
           <v-img
               :src="userProfile.avatar"
               alt="Avatar"
@@ -22,19 +22,30 @@
             value="3"
             dense
         ></v-rating>
-        <v-col class="text-end text-body-2" style="color: gray">
+        <v-col class="text-end text-body-2 d-sm-block d-none" style="color: gray">
           {{ comment.time }}
         </v-col>
       </v-row>
       <v-row class="mt-3">
         <v-card elevation="0">
           <v-card-title class="font-weight-black text-body-1">{{ comment.title }}</v-card-title>
-          <v-card-text>
+          <v-card-text class="pb-0">
             <div v-html="useMarkdown(comment.comment)">
             </div>
-            <v-subheader>
+            <v-subheader class="mt-sm-0 my-8 my-sm-0">
               <v-row>
-                <v-col class="pl-1">
+                <v-col
+                    v-for="(name, index) in statics.scoreNames" :key="index"
+                    cols="6"
+                    sm="2"
+                    class="pl-0 py-1 pr-sm-15 d-flex"
+                >
+                  <div class="d-flex flex-row text-no-wrap align-center">
+                    <div class="font-weight-bold text-body-2 px-sm-3 pr-1">{{name}}</div>
+                    <div>{{ comment.score[index].toFixed(1) }}</div>
+                  </div>
+                </v-col>
+                <v-col class="pl-1 pr-sm-15" cols="6" sm="2" order="5" order-sm="first">
                   <div class="d-flex">
                     <v-btn
                         icon
@@ -48,13 +59,8 @@
                     </div>
                   </div>
                 </v-col>
-                <v-col
-                    v-for="(name, index) in statics.scoreNames" :key="index"
-                >
-                  <div class="d-flex flex-row text-no-wrap">
-                    <div class="font-weight-bold text-body-2 px-3">{{name}}</div>
-                    <div>{{ comment.score[index].toFixed(1) }}</div>
-                  </div>
+                <v-col class="d-sm-none py-1 text-body-2 d-flex align-center flex-row-reverse" cols="6" sm="0" order="last" style="color: gray">
+                  {{ comment.time }}
                 </v-col>
               </v-row>
             </v-subheader>
