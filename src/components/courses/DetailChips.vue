0326a6b7e9810c21a0c5f2df042db48cbe2d874a
Clarivy <zqc.sc.hn@gmail.com>
2022-02-08 14:19:37 +0800

    Change color of Chips

--- src/components/courses/DetailChips.vue
+++ src/components/courses/DetailChips.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <v-chip class="type-chip" color="success" outlined small>4学分</v-chip>
-    <v-chip class="type-chip ml-3" color="success" outlined small>64课时</v-chip>
+    <v-chip class="type-chip" color="green lighten-3" outlined small>4学分</v-chip>
+    <v-chip class="type-chip ml-3" color="green lighten-3" outlined small>64课时</v-chip>
   </div>
 </template>
 
