7b497e8367f1a72a3846d4ba7a7d3951913be135
wenxuanjun <wendster@163.com>
2022-03-23 02:05:36 +0800

    Fix: Markdown styles
    New: Pangu

--- src/composables/global/useMarkdown.js
+++ src/composables/global/useMarkdown.js
@@ -1,8 +1,9 @@
 import MarkdownIt from 'markdown-it'
+import pangu from 'markdown-it-pangu'
 import prism from 'markdown-it-prism'
 
 export default (resultText) => {
   const parser = new MarkdownIt()
-  parser.use(prism)
+  parser.use(pangu).use(prism)
   return parser.render(resultText)
 }
