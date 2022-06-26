c160cbd79d52c3d44eb95e41c305bdd7a8fcb49d
wenxuanjun <wendster@163.com>
2022-06-26 09:40:54 +0800

    Fix the query partly in an ugly way

--- src/composables/global/useFetching.js
+++ src/composables/global/useFetching.js
@@ -2,9 +2,8 @@ import { useQuery } from "vue-query"
 import axios from "axios"
 import Config from 'Config'
 
-export default (key, address, returnFunction = (result) => result) => {
-    return useQuery(key, async () => {
-        const response = await axios.get(Config.serverUrl + address)
-        return returnFunction(response.data)
-    })
-}
+export default (queryKey, address) => (
+    useQuery(queryKey, () => (
+        axios.get(Config.serverUrl + address).then((result) => result.data)
+    ))
+)
