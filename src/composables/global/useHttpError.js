750406ac8c813ced18a89fa3523a7876ed09e447
yanglinshu <nicolasvandarkholme@outlook.com>
2022-06-29 21:58:38 +0800

    Update useHttpError.

--- src/composables/global/useHttpError.js
+++ src/composables/global/useHttpError.js
@@ -1,15 +1,16 @@
 const isInvalidRequest = (response) => {
-  return /^40\d+$/.test(new String(response.status))
+  return response && /^40\d+$/.test(new String(response.status))
 }
 
 const isBackendError = (response) => {
-  return /^50\d+$/.test(new String(response.status))
+  return response && /^50\d+$/.test(new String(response.status))
 }
 
 const isNetworkError = (response) => {
-  return (!isBackendError(response))
-    && (!isInvalidRequest(response))
-    && response.status != 200
+  return (!response) || 
+    ((!isBackendError(response))
+      && (!isInvalidRequest(response))
+      && response.status != 200)
 }
 
 export { isInvalidRequest, isBackendError, isNetworkError }
