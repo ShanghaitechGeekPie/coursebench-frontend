5ccd62eab133c757c7e605b5bf043a70adf826c8
yanglinshu <nicolasvandarkholme@outlook.com>
2022-07-29 21:32:43 +0800

    Verify page finish.

--- src/views/Verify.vue
+++ src/views/Verify.vue
@@ -1,19 +1,45 @@
 <template>
-  <div v-if="breakpoint.xsOnly">
+  <div
+    v-if="status.loading"
+    :style="
+      breakpoint.xsOnly
+        ? {}
+        : { background: theme.isDark ? '' : '#f9f9f9', 'min-height': '100%' }
+    "
+  >
+    <v-sheet
+      v-if="!breakpoint.xsOnly"
+      :color="theme.isDark ? statics.backgroundDark : statics.backgroundLight"
+      :height="breakpoint.xsOnly ? 91 : 360"
+    ></v-sheet>
+    <v-overlay :value="status.loading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+  </div>
+  <div v-else-if="breakpoint.xsOnly">
     <div class="main-card" style="min-width: 300px">
       <div class="d-flex justify-center">
-        <v-icon size="160" color="success">
+        <v-icon v-if="status.errorMessage == ''" size="160" color="success">
           {{ statics.icons.mdiCheckboxMarkedCircleOutline }}
+        </v-icon>
+        <v-icon v-else size="160" color="error">
+          {{ statics.icons.mdiCloseCircleOutline }}
         </v-icon>
       </div>
       <div>
-        <div>
-          <div class="text-h4 pt-6">电子邮箱验证成功!</div>
-          <div class="pt-6 word-wrap">
+        <div v-if="status.errorMessage == ''">
+          <div class="text-h4 pt-6 text-center">电子邮箱验证成功!</div>
+          <div class="pt-6 word-wrap text-center">
             您的账号已经注册成功，欢迎您使用本系统。
           </div>
         </div>
-        <div class="d-flex justify-center pt-16">
+        <div v-else>
+          <div class="text-h4 pt-6 text-center">发生错误!</div>
+          <div class="pt-6 word-wrap text-center">
+            {{ status.errorMessage }}，请稍后重试。
+          </div>
+        </div>
+        <div v-if="!status.loading" class="d-flex justify-center pt-16">
           <v-btn
             width="90px"
             elevation="1"
@@ -34,24 +60,46 @@
       :color="theme.isDark ? statics.backgroundDark : statics.backgroundLight"
       :height="breakpoint.xsOnly ? 91 : 360"
     ></v-sheet>
-    <div class="main-card">
-      <v-card width="600px" height="400px" elevation="2">
-        <div class="pa-16 d-flex justify-space-between" style="height: 100%">
-          <div style="height: 100%">
-            <v-icon size="128" color="success">
-              {{ statics.icons.mdiCheckboxMarkedCircleOutline }}
-            </v-icon>
-          </div>
-          <div
-            class="pl-6 d-flex justify-space-between flex-column"
-            style="max-width: 344px; height: 100%"
-          >
-            <div>
-              <div class="text-h4 pt-6">电子邮箱验证成功!</div>
-              <div class="pt-6 word-wrap">
-                您的账号已经注册成功，欢迎您使用本系统。
+    <v-fade-transition>
+      <div class="main-card">
+        <v-card
+          width="600px"
+          height="400px"
+          elevation="2"
+          class="d-flex justify-space-between flex-column"
+        >
+          <div class="px-16 pt-16 d-flex">
+            <div style="height: 100%">
+              <v-icon
+                v-if="status.errorMessage == ''"
+                size="128"
+                color="success"
+              >
+                {{ statics.icons.mdiCheckboxMarkedCircleOutline }}
+              </v-icon>
+              <v-icon v-else size="128" color="error">
+                {{ statics.icons.mdiCloseCircleOutline }}
+              </v-icon>
+            </div>
+            <div
+              class="pl-6 d-flex justify-space-between flex-column"
+              style="max-width: 344px; height: 100%"
+            >
+              <div v-if="status.errorMessage == ''">
+                <div class="text-h4 pt-6">电子邮箱验证成功!</div>
+                <div class="pt-6 word-wrap">
+                  您的账号已经注册成功，欢迎您使用本系统。
+                </div>
+              </div>
+              <div v-else>
+                <div class="text-h4 pt-6">发生错误!</div>
+                <div class="pt-6 word-wrap">
+                  {{ status.errorMessage }}，请稍后重试。
+                </div>
               </div>
             </div>
+          </div>
+          <div class="px-16 pb-16 d-flex justify-end">
             <div class="d-flex justify-end pb-6 pr-4">
               <v-btn
                 width="90px"
@@ -63,9 +111,9 @@
               </v-btn>
             </div>
           </div>
-        </div>
-      </v-card>
-    </div>
+        </v-card>
+      </div>
+    </v-fade-transition>
   </div>
 </template>
 
@@ -74,8 +122,8 @@ import useVerify from "@/composables/users/useVerify";
 
 export default {
   setup() {
-    const { statics } = useVerify();
-    return { statics };
+    const { statics, status } = useVerify();
+    return { statics, status };
   },
   data() {
     return {
@@ -92,30 +140,6 @@ export default {
   left: 50%;
   top: 45%;
   transform: translate(-50%, -50%);
-}
-
-.wrapper-light {
-  min-height: 100%;
-  background-image: linear-gradient(
-    to top,
-    #f9f9f9,
-    #f0f2fa,
-    #e1eefc,
-    #cdeafa,
-    #b8e7f4
-  );
-}
-
-.wrapper-dark {
-  min-height: 100%;
-  background-image: linear-gradient(
-    to bottom,
-    #272727,
-    #313131,
-    #3b3b3b,
-    #464546,
-    #515050
-  );
 }
 
 .word-wrap {
