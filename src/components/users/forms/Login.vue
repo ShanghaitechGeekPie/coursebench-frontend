03fd355afe3f3f4a4d6f80d60bcd6b2aa5e49529
yanglinshu <nicolasvandarkholme@outlook.com>
2022-09-03 00:12:35 +0800

    Rewrite and clean up login

--- src/components/users/forms/Login.vue
+++ src/components/users/forms/Login.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-card height="338px" :loading="formStatus.loading"
-    style="overflow-y: hidden;"
+  <v-card
+    height="338px"
+    :loading="formStatus.loading"
+    style="overflow-y: hidden"
+    :class="
+      breakpoint.name === 'xs' ? '' : 'd-flex justify-space-between flex-column'
+    "
   >
     <v-card-title
       class="pt-6 pt-sm-11 px-6 px-sm-11 pb-0 d-flex justify-space-between"
@@ -17,109 +22,137 @@
         </v-btn>
       </div>
     </v-card-title>
-    <v-window v-model="formStatus.windowStep">
-      <div
-        class="px-6 px-sm-11 py-0 d-flex flex-column justify-space-between"
-        style="height: 100%; max-height: 338px"
-      >
-        <v-window-item :value="0">
-          <div class="font-weight-bold text-h5 pt-2">登录</div>
-          <v-form v-model="formStatus.emailFormValid">
-            <v-card-text class="pa-0">
-              <v-text-field
-                v-model="userData.email"
-                :rules="[formRules.required, formRules.email]"
-                ref="emailTextField"
-              ></v-text-field>
-            </v-card-text>
-          </v-form>
-          <div class="text-body-2 py-1">
-            <span>没有账户？</span>
-            <span
-              class="inline-link"
-              @click="closeDialog('login'), openDialog('register')"
-              >创建一个</span
+    <v-window v-model="formStatus.windowStep" touchless
+      style="height: 100%"
+    >
+      <v-window-item :value="0" style="height: 100%">
+        <v-card-text
+          class="px-6 px-sm-11 py-0 d-flex justify-space-between flex-column"
+          style="height: 100%; max-height: 338px"
+        >
+          <div>
+            <div class="font-weight-bold text-h5 pt-2">登录</div>
+            <v-form v-model="formStatus.emailFormValid">
+              <v-card-text class="pa-0">
+                <v-text-field
+                  v-model="userData.email"
+                  :rules="[formRules.required, formRules.email]"
+                  ref="emailTextField"
+                ></v-text-field>
+              </v-card-text>
+            </v-form>
+            <div class="text-body-2 py-1">
+              <span>没有账户？</span>
+              <span
+                class="inline-link"
+                @click="closeDialog('login'), openDialog('register')"
+                >创建一个</span
+              >
+            </div>
+          </div>
+          <div
+            class="px-0 pt-0 pb-6 pb-sm-11 d-flex justify-end"
+            v-if="formStatus.windowStep !== 2"
+          >
+            <v-btn
+              @click="clickNextStep()"
+              elevation="0"
+              class="px-8"
+              color="primary"
             >
-          </div>
-        </v-window-item>
-        <v-window-item :value="1">
-          <div class="text-body-2 py-2 overflow-ellipsis">
-            <v-btn icon x-small @click="clickLastStep()">
-              <v-icon>
-                {{ statics.icons.mdiArrowLeft }}
-              </v-icon>
+              下一步
             </v-btn>
-            {{ userData.email }}
           </div>
-          <div class="font-weight-bold text-h5">输入密码</div>
-          <v-form v-model="formStatus.passwordFormValid">
-            <v-card-text class="pa-0">
-              <v-text-field
-                v-model="userData.password"
-                :type="formStatus.passwordVisible ? 'text' : 'password'"
-                :rules="[formRules.required, formRules.password]"
-                ref="passwordTextField"
-                :append-icon="
-                  formStatus.passwordVisible
-                    ? statics.icons.mdiEyeOff
-                    : statics.icons.mdiEye
-                "
-                @click:append="
-                  formStatus.passwordVisible = !formStatus.passwordVisible
-                "
-              ></v-text-field>
-            </v-card-text>
-          </v-form>
-          <div class="text-body-2 py-1">
-            <span class="inline-link" @click="">忘记了密码？</span>
+        </v-card-text>
+      </v-window-item>
+      <v-window-item :value="1" style="height: 100%">
+        <v-card-text           
+          class="px-6 px-sm-11 py-0 d-flex justify-space-between flex-column"
+          style="height: 100%; max-height: 338px"
+        >
+          <div>
+            <div class="text-body-2 py-2 overflow-ellipsis">
+              <v-btn icon x-small @click="clickLastStep()">
+                <v-icon>
+                  {{ statics.icons.mdiArrowLeft }}
+                </v-icon>
+              </v-btn>
+              {{ userData.email }}
+            </div>
+            <div class="font-weight-bold text-h5">输入密码</div>
+            <v-form v-model="formStatus.passwordFormValid">
+              <v-card-text class="pa-0">
+                <v-text-field
+                  v-model="userData.password"
+                  :type="formStatus.passwordVisible ? 'text' : 'password'"
+                  :rules="[formRules.required, formRules.password]"
+                  ref="passwordTextField"
+                  :append-icon="
+                    formStatus.passwordVisible
+                      ? statics.icons.mdiEyeOff
+                      : statics.icons.mdiEye
+                  "
+                  @click:append="
+                    formStatus.passwordVisible = !formStatus.passwordVisible
+                  "
+                ></v-text-field>
+              </v-card-text>
+            </v-form>
+            <div class="text-body-2 py-1">
+              <span class="inline-link" @click="">忘记了密码？</span>
+            </div>
           </div>
-        </v-window-item>
-        <v-window-item :value="2">
-          <div class="text-body-2 py-2 overflow-ellipsis">
-            <v-btn icon x-small @click="clickLastStep()">
-              <v-icon>
-                {{ statics.icons.mdiArrowLeft }}
-              </v-icon>
+          <div
+            class="px-0 pt-0 pb-6 pb-sm-11 d-flex justify-end"
+            v-if="formStatus.windowStep !== 2"
+          >
+            <v-btn
+              @click="clickNextStep()"
+              elevation="0"
+              class="px-8"
+              color="primary"
+            >
+              下一步
             </v-btn>
-            {{ userData.email }}
           </div>
-          <div class="text-body-2 py-0">
-            <span>看不清？</span>
-            <span class="inline-link" @click="getCaptcha()">点击刷新</span>
-          </div>
-          <!-- <div class="font-weight-bold text-h5">输入验证码</div> -->
-          <v-card-text class="px-0 pt-2 pb-0">
-            <div class="d-flex justify-center">
+        </v-card-text>
+      </v-window-item>
+      <v-window-item :value="2" style="height: 100%">
+        <v-card-text           
+          class="px-6 px-sm-11 py-0 d-flex justify-space-between flex-column"
+          style="height: 100%; max-height: 338px"
+        > 
+          <div>
+            <div class="text-body-2 py-2 overflow-ellipsis">
+              <v-btn icon x-small @click="clickLastStep()">
+                <v-icon>
+                  {{ statics.icons.mdiArrowLeft }}
+                </v-icon>
+              </v-btn>
+              {{ userData.email }}
+            </div>
+            <div class="text-body-2 py-0">
+              <span>看不清？</span>
+              <span class="inline-link" @click="getCaptcha()">点击刷新</span>
+            </div>
+            <div class="d-flex justify-center pt-3">
               <div style="width: 200px" class="d-flex justify-center">
                 <v-img
                   :src="`data:image/png;base64,${formStatus.captchaBase64}`"
                 ></v-img>
               </div>
-            </div>
-            <div class="pt-2">
-              <v-otp-input
-                length="6"
-                v-model="userData.captcha"
-                @finish="doLogin"
-              ></v-otp-input>
-            </div>
-          </v-card-text>
-        </v-window-item>
-      </div>
+            </div>  
+          </div>
+          <div class="pb-6 pb-sm-11">
+            <v-otp-input
+              length="6"
+              v-model="userData.captcha"
+              @finish="doLogin"
+            ></v-otp-input>
+          </div>                  
+        </v-card-text>
+      </v-window-item>
     </v-window>
-    <v-card-actions
-      class="px-6 px-sm-11 pb-6 pb-sm-11 py-1 d-flex justify-end"
-      v-if="formStatus.windowStep !== 2"
-    >
-      <v-btn
-        @click="clickNextStep()"
-        elevation="0"
-        class="px-8"
-        color="primary"
-      >
-        下一步
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
@@ -167,10 +200,7 @@ export default {
           useAfterRender(
             () => {
               this.$refs.passwordTextField.focus();
-            },
-            {
-              timeout: 300,
-            }
+            }, { retry: true, timeout: 300 }
           );
         }
       } else if (this.formStatus.windowStep === 1) {
@@ -185,7 +215,7 @@ export default {
   mounted() {
     useAfterRender(() => {
       this.$refs.emailTextField.focus();
-    });
+    }, { retry: true });
   },
 };
 </script>
--- src/components/users/forms/Register.vue
+++ src/components/users/forms/Register.vue
@@ -1,129 +1,144 @@
 <template>
-  <v-card>
-    <v-card-title>注册</v-card-title>
+  <v-card height="338px" :loading="formStatus.loading"
+    style="overflow-y: hidden;"
+  >
+    <v-card-title
+      class="pt-6 pt-sm-11 px-6 px-sm-11 pb-0 d-flex justify-space-between"
+    >
+      <div>!!LOGO!!</div>
+      <div>
+        <v-btn
+          icon
+          style="transform: translate(0, -1px)"
+          @click="closeDialog('login')"
+          v-if="breakpoint.name === 'xs'"
+        >
+          <v-icon>{{ statics.icons.mdiWindowClose }}</v-icon>
+        </v-btn>
+      </div>
+    </v-card-title>
     <v-window v-model="formStatus.windowStep">
-      <v-window-item :value="0">
-        <v-form v-model="formStatus.emailFormValid">
-          <v-card-text class="px-6">
-            <v-text-field
-              v-model="userData.email"
-              label="邮箱"
-              :rules="[formRules.required, formRules.email]"
-            ></v-text-field>
-          </v-card-text>
-        </v-form>
-      </v-window-item>
-      <v-window-item :value="1">
-        <v-form v-model="formStatus.passwordFormValid">
+      <div
+        class="px-6 px-sm-11 py-0 d-flex flex-column justify-space-between"
+        style="height: 100%; max-height: 338px"
+      >      
+        <v-window-item :value="0">
+          <div class="font-weight-bold text-h5 pt-2">登录</div>
+          <v-form v-model="formStatus.emailFormValid">
+            <v-card-text class="pa-0">
+              <v-text-field
+                v-model="userData.email"
+                :rules="[formRules.required, formRules.email]"
+                ref="emailTextField"
+              ></v-text-field>
+            </v-card-text>
+          </v-form>
+          <div class="text-body-2 py-1">
+            <span>没有账户？</span>
+            <span
+              class="inline-link"
+              @click="closeDialog('login'), openDialog('register')"
+              >创建一个</span
+            >
+          </div>
+        </v-window-item>
+        <v-window-item :value="1">
+          <v-form v-model="formStatus.passwordFormValid">
+            <v-card-text class="px-6">
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="userData.username"
+                    label="用户名"
+                    :rules="[formRules.required, formRules.username]"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select
+                    v-model="userData.year"
+                    :items="userData.yearItems"
+                    label="入学时间"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select
+                    v-model="userData.grade"
+                    :items="userData.gradeItems"
+                    label="年级"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="userData.password"
+                    label="密码"
+                    type="password"
+                    :rules="[formRules.required, formRules.password]"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="userData.confirmPassword"
+                    label="确认密码"
+                    type="password"
+                    :rules="[
+                      formRules.required,
+                      (value) =>
+                        value === userData.password || '两次输入的密码不一致',
+                    ]"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-form>
+        </v-window-item>
+        <v-window-item :value="2">
           <v-card-text class="px-6">
             <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="userData.username"
-                  label="用户名"
-                  :rules="[formRules.required, formRules.username]"
-                ></v-text-field>
+              <v-col cols="12" sm="6" offset-sm="3">
+                <v-img
+                  :src="`data:image/png;base64,${formStatus.captchaBase64}`"
+                ></v-img>
               </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  v-model="userData.year"
-                  :items="userData.yearItems"
-                  label="入学时间"
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  v-model="userData.grade"
-                  :items="userData.gradeItems"
-                  label="年级"
-                ></v-select>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="userData.password"
-                  label="密码"
-                  type="password"
-                  :rules="[formRules.required, formRules.password]"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="userData.confirmPassword"
-                  label="确认密码"
-                  type="password"
-                  :rules="[
-                    formRules.required,
-                    (value) =>
-                      value === userData.password || '两次输入的密码不一致',
-                  ]"
-                ></v-text-field>
+              <v-col cols="12" sm="8" offset-sm="2">
+                <v-otp-input length="6" @finish="doRegister"></v-otp-input>
               </v-col>
             </v-row>
+            <v-overlay absolute :value="formStatus.loading">
+              <v-progress-circular
+                indeterminate
+                color="primary"
+              ></v-progress-circular>
+            </v-overlay>
           </v-card-text>
-        </v-form>
-      </v-window-item>
-      <v-window-item :value="2">
-        <v-card-text class="px-6">
-          <v-row>
-            <v-col cols="12" sm="6" offset-sm="3">
-              <v-img
-                :src="`data:image/png;base64,${formStatus.captchaBase64}`"
-              ></v-img>
-            </v-col>
-            <v-col cols="12" sm="8" offset-sm="2">
-              <v-otp-input length="6" @finish="doRegister"></v-otp-input>
-            </v-col>
-          </v-row>
-          <v-overlay absolute :value="formStatus.loading">
-            <v-progress-circular
-              indeterminate
-              color="primary"
-            ></v-progress-circular>
-          </v-overlay>
-        </v-card-text>
-      </v-window-item>
-      <v-window-item :value="3">
-        <v-card-text class="px-6 text-center">
-          <v-img
-            class="mb-4"
-            contain
-            height="128"
-            src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-          ></v-img>
-          <h3 class="text-h6 font-weight-light">欢迎来到 CourseBench</h3>
-          <span class="text-caption grey--text"
-            >激活邮件已发送至您的邮箱，请点击其中的激活链接以激活您的帐号</span
-          >
-        </v-card-text>
-      </v-window-item>
+        </v-window-item>
+        <v-window-item :value="3">
+          <v-card-text class="px-6 text-center">
+            <v-img
+              class="mb-4"
+              contain
+              height="128"
+              src="https://cdn.vuetifyjs.com/images/logos/v.svg"
+            ></v-img>
+            <h3 class="text-h6 font-weight-light">欢迎来到 CourseBench</h3>
+            <span class="text-caption grey--text"
+              >激活邮件已发送至您的邮箱，请点击其中的激活链接以激活您的帐号</span
+            >
+          </v-card-text>
+        </v-window-item>
+      </div>
     </v-window>
-    <v-divider></v-divider>
-    <v-card-actions>
+    <v-card-actions
+      class="px-6 px-sm-11 pb-6 pb-sm-11 py-1 d-flex justify-end"
+      v-if="formStatus.windowStep !== 2"
+    >
       <v-btn
-        v-if="formStatus.windowStep != 0 && formStatus.windowStep != 3"
-        text
-        @click="formStatus.windowStep -= 1"
-        >上一步</v-btn
-      >
-      <v-spacer></v-spacer>
-      <v-btn
-        v-if="formStatus.windowStep <= 1"
-        @click="formStatus.windowStep += 1"
-        text
-        :disabled="
-          formStatus.windowStep === 0
-            ? !formStatus.emailFormValid
-            : !formStatus.passwordFormValid
-        "
+        @click="clickNextStep()"
+        elevation="0"
+        class="px-8"
+        color="primary"
       >
         下一步
       </v-btn>
-      <v-btn
-        v-if="formStatus.windowStep == 3"
-        color="primary"
-        @click="closeDialog('register')"
-        >完成</v-btn
-      >
     </v-card-actions>
   </v-card>
 </template>
@@ -137,8 +152,14 @@ export default {
   setup() {
     const { formRules } = useForms();
     const closeDialog = inject("closeDialog");
+    const openDialog = inject("openDialog");
     const { userData, formStatus, doRegister } = useRegister();
     return { userData, formStatus, formRules, closeDialog, doRegister };
+  },
+  data() {
+    return {
+      breakpoint: this.$vuetify.breakpoint,
+    };
   },
 };
 </script>
