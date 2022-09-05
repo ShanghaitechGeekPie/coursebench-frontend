f602e6d957b77f19be6ed04ebad4e290e2d9d025
yanglinshu <nicolasvandarkholme@outlook.com>
2022-09-05 21:49:18 +0800

    Default pagination is available, but the replacement is buggy

--- src/composables/courses/all/useSelectBar.js
+++ src/composables/courses/all/useSelectBar.js
@@ -7,12 +7,7 @@ export default () => {
 
 
   const statics = {
-    sortKeyItem: ['综合评分', '评价总数', "学分"],
-    orderItem: {
-      "综合评分": ["从高到低", "从低到高"],
-      "评价总数": ["从多到少", "从少到多"], 
-      "学分": ["从多到少", "从少到多"]
-    },
+    ...inject("sortStatics"),
   }
 
   
--- src/composables/users/comment/useSelectBar.js
+++ src/composables/users/comment/useSelectBar.js
@@ -7,11 +7,7 @@ export default () => {
 
 
   const statics = {
-    sortKeyItem: ['发布时间', '修改时间'],
-    orderItem: {
-      "发布时间": ['从后往前', '从前往后'], 
-      "修改时间": ['从后往前', '从前往后']
-    },
+    ...inject("sortStatics"),
   }
   
   
