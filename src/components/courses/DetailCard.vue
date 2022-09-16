b771b7ecdeddf559fbc9e2467e62c3c5158a14a1
Clarivy <zqc.sc.hn@gmail.com>
2022-09-16 11:03:50 +0800

    New: UI redesign of DetailCard

--- src/components/courses/DetailCard.vue
+++ src/components/courses/DetailCard.vue
@@ -1,17 +1,20 @@
 <template>
   <v-scroll-y-transition>
     <v-card
-      class="info-wrp rounded-b-0"
-      :class="screen"
-      color="#11638B"
-      elevation="0"
-      outlined
-      v-show="isShow"
-      max-width="850px"
+        class="info-wrp rounded-b-0"
+        :class="screen"
+        color="#11638B"
+        elevation="0"
+        outlined
+        v-show="isShow"
+        width="800px"
     >
-      <v-row class="pb-1">
+      <v-row class="pb-0">
         <v-col class="px-0" sm="6" cols="12">
-          <div class="white--text text-h5 font-weight-bold pt-8 ml-sm-12 ml-8 single-line-limit pr-3">{{ details.name }}</div>
+          <div class="white--text text-h5 font-weight-bold pt-6 ml-sm-12 ml-8 single-line-limit pr-3">{{
+              details.name
+            }}
+          </div>
           <div class="white--text ml-sm-12 ml-8">
             <span class="subtitle-2 font-weight-bold">
               {{ details.code }}
@@ -19,13 +22,13 @@
               | {{ details.comment_num }}评论
             </span>
           </div>
-<!--          <div class="white&#45;&#45;text subtitle-1 font-weight-bold ml-sm-12 ml-7 pt-3"> 开课单位: {{ details.institute }} </div>-->
+          <!--          <div class="white&#45;&#45;text subtitle-1 font-weight-bold ml-sm-12 ml-7 pt-3"> 开课单位: {{ details.institute }} </div>-->
           <div class="pt-3">
             <div class="white--text subtitle-1 font-weight-bold ml-sm-12 ml-7 ">
               开课单位:
             </div>
             <div class="white--text text-caption font-weight-bold ml-sm-12 ml-7 ">
-                {{ details.institute }}
+              {{ details.institute }}
             </div>
           </div>
           <div>
@@ -42,28 +45,52 @@
                     </span>
             </div>
           </div>
-<!--          <div class="white&#45;&#45;text text-caption font-weight-medium ml-sm-12 ml-7">先修课程: 无</div>-->
-<!--          <div class="white&#45;&#45;text text-caption font-weight-medium ml-sm-12 ml-7">-->
-<!--            英文名：Introduction to Programming-->
-<!--          </div>-->
-<!--          <div class="white&#45;&#45;text text-caption font-weight-medium ml-sm-12 ml-7">学历层次：本科</div>-->
         </v-col>
         <v-divider vertical inset class="my-6 d-none d-sm-block"></v-divider>
-        <v-col class="d-flex flex-column my-auto px-0" sm="6" cols="12">
-          <v-row class="my-2 mx-5">
-            <v-col>
-              <ScoreBoard fontColor="#D4E157" text="课程质量" value="40" cols="3"/>
-            </v-col>
-            <v-col>
-              <ScoreBoard fontColor="pink" text="作业用时" value="93" cols="3"/>
-            </v-col>
-            <v-col>
-              <ScoreBoard fontColor="#00E676" text="考核难度" value="74" cols="3" />
-            </v-col>
-            <v-col>
-              <ScoreBoard fontColor="#66ccff" text="给分情况" value="74" cols="3"/>
-            </v-col>
-          </v-row>
+        <v-col class="mt-lg-6 px-0" sm="6" cols="12">
+          <div class="ml-6">
+            <div
+                class="subtitle-1 white--text font-weight-bold"
+            >
+              Ratings & Reviews
+            </div>
+            <div style="font-family: Arial,serif">
+              <span
+                  class="text-h3 white--text font-weight-bold"
+                  style="font-family: Arial,serif!important;"
+              >
+                4.2
+              </span>
+              <span
+                  class="white--text font-weight-bold"
+              >
+              /5
+              </span>
+            </div>
+            <ScoreBoard class="mb-4"></ScoreBoard>
+            <v-row no-gutters>
+              <v-col class="d-flex" v-for="index in [0, 1, 2, 3]" :key="index" cols="6">
+                <div
+                    class="text-body-2 pr-2 font-weight-bold white--text"
+                    style="min-width: 57px"
+                >
+                  {{ judgeItems[index] }}
+                </div>
+                <div>
+                  <v-chip
+                      x-small
+                      label
+                      :color="scoreInfo[index].color"
+                      class="px-1 mt-n2"
+                  >
+                        <span class="text-caption white--text">{{
+                            scoreInfo[index].label
+                          }}</span>
+                  </v-chip>
+                </div>
+              </v-col>
+            </v-row>
+          </div>
         </v-col>
       </v-row>
     </v-card>
@@ -75,16 +102,17 @@ import ScoreBoard from "@/components/courses/ScoreBoard"
 import DetailChips from "@/components/courses/DetailChips"
 import ReviewDetail from "@/components/courses/ReviewDetail"
 import useDetailCard from "@/composables/courses/comment/useDetailCard";
+import {judgeItems, scoreInfo} from "@/composables/global/useStaticData";
 
 export default {
   name: "DetailCard",
-  components: { ScoreBoard, DetailChips, ReviewDetail},
+  components: {ScoreBoard, DetailChips, ReviewDetail},
   props: {
     details: Object
   },
   setup() {
-    const { teachers } = useDetailCard()
-    return { teachers }
+    const { teachers, statics } = useDetailCard()
+    return { teachers, statics, judgeItems, scoreInfo }
   },
   computed: {
     screen() {
@@ -113,6 +141,7 @@ export default {
 .info-wrp {
   /*background: linear-gradient(70deg, blue, lightcyan);*/
 }
+
 .single-line-limit {
   white-space: nowrap;
   overflow: hidden;
