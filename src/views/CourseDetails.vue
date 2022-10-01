b9ac30c30e3296fbb22d28a2e4a8e194dad62c1f
Clarivy <zqc.sc.hn@gmail.com>
2022-10-01 19:02:59 +0800

    Fix: margin in CourseDetails

--- src/views/CourseDetails.vue
+++ src/views/CourseDetails.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-container class="d-flex justify-center mt-6">
-    <v-row class="d-flex justify-center" style="max-width: 1300px">
-      <v-col sm="11" cols="12" class="pa-sm-3 pa-0">
+  <v-container class="d-flex justify-center mt-6" >
+    <v-row class="d-flex justify-center" style="max-width: 1300px" >
+      <v-col sm="11" cols="12" class="pa-sm-3 pa-0" >
         <v-row class="d-flex justify-center">
-          <v-col cols="11" class="px-0">
+          <v-col cols="11" class="px-0 mb-sm-2">
             <DetailCard
               class="pb-8 pt-2"
               :details="courseDetail"
@@ -15,7 +15,7 @@
               <v-col sm="3" cols="12" class="pt-0 pl-0 pr-sm-3 pr-0"  >
                 <TeacherSelect class="" :style="adoptiveCardPosition"/>
               </v-col>
-              <v-col sm="9" cols="12">
+              <v-col sm="9" cols="12" class="pl-sm-5">
                 <v-row>
                   <v-col cols="12" class="px-0 pt-0 pb-1">
                     <writing-box :disable-writing="disableWriting"></writing-box>
@@ -28,6 +28,9 @@
                   >
                     <CourseCommentCard :comment="comment">
                     </CourseCommentCard>
+                  </v-col>
+                  <v-col v-if="selectedComment.length === 0" class="d-flex justify-center" cols="12">
+                    <nothing class="pt-10" :text="'还没有评论...'"></nothing>
                   </v-col>
                 </v-row>
               </v-col>
@@ -45,9 +48,10 @@ import useCourseDetails from "@/composables/courses/comment/useCourseDetails";
 import CourseCommentCard from "@/components/courses/CourseCommentCard";
 import TeacherSelect from "@/components/courses/FilterBox";
 import WritingBox from "@/components/courses/WritingBox";
+import Nothing from "@/components/global/Nothing";
 
 export default {
-  components: { WritingBox, DetailCard, TeacherSelect, CourseCommentCard },
+  components: {Nothing, WritingBox, DetailCard, TeacherSelect, CourseCommentCard },
   setup() {
     const { commentText, status, courseDetail, selectedTeachers, groups, global } = useCourseDetails();
     return { commentText, status, courseDetail, selectedTeachers, groups, global};
