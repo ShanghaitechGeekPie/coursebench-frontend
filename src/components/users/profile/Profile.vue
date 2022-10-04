87a074eb0177332a4a7dcea8fc7a199e010bb1e0
yanglinshu <nicolasvandarkholme@outlook.com>
2022-10-04 14:06:47 +0800

    Change the change avatar into d-none so that ctfer can upload their avatars

--- src/components/users/profile/Profile.vue
+++ src/components/users/profile/Profile.vue
@@ -12,7 +12,7 @@
           :size="$vuetify.breakpoint.name === 'xs' ? 110 : 120"
           :outlined="$vuetify.breakpoint.mdAndDown"
         >
-        <v-overlay absolute v-if="isSelf && false">
+        <v-overlay absolute v-if="isSelf" class="d-none">
           <div>
             <v-file-input
               dense
