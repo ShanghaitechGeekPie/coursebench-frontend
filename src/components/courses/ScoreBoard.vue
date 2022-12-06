5073f69e752e0a98c3c1c17263c209e485c19e4b
yanglinshu <nicolasvandarkholme@outlook.com>
2022-12-06 22:08:10 +0800

    Adjust layout in course detail

--- src/components/courses/ScoreBoard.vue
+++ src/components/courses/ScoreBoard.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <div v-for="index in [1, 2, 3, 4, 5]" >
-      <ScoreLine :starNum="6 - index" :percentage="Math.round(starPercent[5 - index] * 100)" :color="scoreInfo[6 - index].color" style="max-height: 13px"></ScoreLine>
+    <div v-for="index in [1, 2, 3, 4, 5]">
+      <ScoreLine
+        :starNum="6 - index"
+        :percentage="Math.round(starPercent[5 - index] * 100)"
+        :color="scoreInfo[6 - index].color"
+        style="max-height: 13px"
+      ></ScoreLine>
     </div>
   </div>
 </template>
 
 <script>
-
 import ScoreLine from "@/components/courses/ScoreLine";
-import {scoreInfo} from "@/composables/global/useStaticData";
+import { scoreInfo } from "@/composables/global/useStaticData";
 
 export default {
-  components: {ScoreLine},
+  components: { ScoreLine },
   props: {
-    starPercent: Array
+    starPercent: Array,
   },
   setup() {
-    return { scoreInfo }
-  }
-}
+    return { scoreInfo };
+  },
+};
 </script>
 
 <style scoped>
