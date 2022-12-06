370d112dd56c56f0d5c2a9d3532f40683a50a640
yanglinshu <nicolasvandarkholme@outlook.com>
2022-12-06 15:55:35 +0800

    Fix padding bug

--- src/components/courses/DetailCard.vue
+++ src/components/courses/DetailCard.vue
@@ -7,7 +7,7 @@
         color=""
     >
       <v-row class="pb-0">
-        <v-col class="px-0 pl-sm-12 pl-8 " sm="6" cols="12" v-if="details.name">
+        <v-col class="px-0 pl-sm-12 pl-8 pt-0" sm="6" cols="12" v-if="details.name">
           <div class="text-h6 pt-6 single-line-limit pr-3" style="font-weight: bold">{{
               details.name
             }}
