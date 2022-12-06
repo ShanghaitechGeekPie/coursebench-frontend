4b90c023980ddb47ddf696372fc79bec5c87a79c
yanglinshu <nicolasvandarkholme@outlook.com>
2022-12-06 22:36:49 +0800

    Complete loader in course detail

---
+++ src/components/teachers/loader/DetailCardLoader.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-card class="ma-0" elevation="0" outlined>
+    <v-card-text>
+      <v-row class="">
+        <v-col class="" md="6" cols="12">
+          <div
+            class="mr-md-6 d-flex flex-column justify-space-between"
+            style="height: 100%"
+          >
+            <div>
+              <v-sheet
+                class="mt-1 mb-0 mb-lg-0"
+                color="#e2e2e2"
+                height="38px"
+                width="50%"
+              ></v-sheet>
+              <div class="pt-2">
+                <v-sheet color="#e2e2e2" width="40%" height="18px"></v-sheet>
+              </div>
+            </div>
+            <div class="">
+              <div class="pt-8">
+                <div class="subtitle-1 font-weight-regular pb-1">
+                  <v-sheet color="#e2e2e2" width="20%" height="18px"></v-sheet>
+                </div>
+                <div class="text-caption font-weight-regular pt-1">
+                  <v-sheet color="#e2e2e2" width="25%" height="14px"></v-sheet>
+                </div>
+              </div>
+              <div class="pt-3">
+                <div class="subtitle-1 font-weight-regular pb-1">
+                  <v-sheet color="#e2e2e2" width="20%" height="18px"></v-sheet>
+                </div>
+                <div class="text-caption font-weight-regular pt-1">
+                  <v-sheet color="#e2e2e2" width="60%" height="14px"></v-sheet>
+                </div>
+              </div>
+            </div>
+          </div>
+        </v-col>
+        <v-divider
+          vertical
+          inset
+          class="mb-2 mt-3 d-none d-md-block"
+        ></v-divider>
+        <v-col class="" md="6" cols="12">
+          <div class="ml-md-6">
+            <div class="subtitle-1 font-weight-regular pb-1">
+              <v-sheet color="#e2e2e2" width="36%" height="18px"></v-sheet>
+            </div>
+            <div style="font-family: Arial, serif" class="pt-md-2 mb-2">
+              <v-sheet
+                class="mt-1 mb-1 mb-lg-0"
+                color="#e2e2e2"
+                height="38px"
+                width="28%"
+              ></v-sheet>
+            </div>
+            <div>
+              <div v-for="index in 4" :key="index">
+                <v-sheet
+                  class="my-2"
+                  color="#e2e2e2"
+                  width="100%"
+                  height="12px"
+                ></v-sheet>
+              </div>
+            </div>
+            <v-row no-gutters class="pt-md-6 pt-4">
+              <v-sheet
+                class="my-1"
+                color="#e2e2e2"
+                width="100%"
+                height="16px"
+              ></v-sheet>
+              <v-sheet
+                class="mt-1"
+                color="#e2e2e2"
+                width="100%"
+                height="16px"
+              ></v-sheet>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
--- src/components/teachers/loader/FilterBoxLoader.vue
+++ src/components/teachers/loader/FilterBoxLoader.vue
@@ -5,35 +5,54 @@
         class="pr-lg-4 pl-lg-4 pt-lg-4 pb-lg-0 pr-4 pl-3 py-3"
         style="max-width: 140px; min-width: 130px"
       >
-          <v-sheet class="mt-1" color="#e2e2e2" height="13px" width="60%"></v-sheet>
-          <v-sheet class="mt-2 mb-0 mb-lg-2" color="#e2e2e2" height="29px"></v-sheet>
+        <v-sheet
+          class="mt-1"
+          color="#e2e2e2"
+          height="13px"
+          width="60%"
+        ></v-sheet>
+        <v-sheet
+          class="mt-2 mb-0 mb-lg-2"
+          color="#e2e2e2"
+          height="29px"
+        ></v-sheet>
       </div>
       <div
         class="pr-lg-4 pl-lg-4 pb-lg-4 pt-lg-0 pl-1 pr-3 py-3"
         style="max-width: 140px; min-width: 130px"
       >
-        <v-sheet class="mt-lg-2 mt-6 mb-0 mb-lg-0" color="#e2e2e2" height="29px"></v-sheet>
+        <v-sheet
+          class="mt-lg-2 mt-6 mb-0 mb-lg-0"
+          color="#e2e2e2"
+          height="29px"
+        ></v-sheet>
       </div>
     </v-row>
     <div class="mb-4 mt-2">
       <div
         class="font-weight-regular subtitle-2 mx-2 grey--text text--darken-1"
-      >        
-        <v-sheet class="mt-1 ml-2 ml-lg-3" color="#e2e2e2" height="13px" width="20%"></v-sheet>
+      >
+        <v-sheet
+          class="mt-1 ml-2 ml-lg-3"
+          color="#e2e2e2"
+          height="13px"
+          width="20%"
+        ></v-sheet>
       </div>
-      <div class="ml-5">
-
+      <div class="ml-4">
+        <v-sheet
+          class="mt-3"
+          color="#e2e2e2"
+          height="25px"
+          width="80%"
+        ></v-sheet>
       </div>
     </div>
   </v-card>
 </template>
 
 <script>
-
-
-export default {
-
-};
+export default {};
 </script>
 
 <style scoped></style>
