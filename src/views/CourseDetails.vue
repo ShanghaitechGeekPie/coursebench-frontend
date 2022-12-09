4964905d10414c77e5c654418b304c9cb2ab78b1
yanglinshu <nicolasvandarkholme@outlook.com>
2022-12-09 13:07:39 +0800

    Fix double scrollbar

--- src/views/CourseDetails.vue
+++ src/views/CourseDetails.vue
@@ -2,77 +2,79 @@
   <v-container class="d-flex justify-center mt-6 mt-sm-13">
     <v-row class="d-flex justify-center" style="max-width: 1300px">
       <v-col sm="11" cols="12" class="pa-sm-3 pa-0">
-        <v-row class="d-flex justify-center">
-          <v-col cols="11" class="px-0 mb-3">
-            <DetailCard
-              :details="courseDetail"
-              :comments="commentText"
-              v-if="!status.commentLoading && !status.detailLoading"
-            />
-            <DetailCardLoader v-else />
-          </v-col>
-          <v-col cols="11">
-            <v-row>
-              <v-col lg="3" cols="12" class="pt-0 pl-0 pr-lg-4 pr-0">
-                <FilterBox
-                  :style="adoptiveCardPosition"
-                  v-if="!status.commentLoading && !status.detailLoading"
-                />
-                <FilterBoxLoader v-else />
-              </v-col>
-              <v-col lg="9" cols="12" class="pl-lg-5">
-                <v-row>
-                  <v-col
-                    cols="12"
-                    class="px-0 pt-0 pb-3"
-                    v-if="
-                      !status.commentLoading &&
-                      !status.detailLoading &&
-                      global.isLogin
-                    "
-                  >
-                    <WritingCard />
-                  </v-col>
-                  <v-col
-                    class="pr-0 pl-0 pr-0 py-0"
-                    v-for="(comment, index) in selectedComment"
-                    :key="comment.id"
-                    cols="12"
-                  >
-                    <CourseCommentCard
-                      :comment="comment"
-                      v-if="!status.commentLoading && !status.detailLoading"
+        <v-container class="px-0 mt-n3">
+          <v-row class="d-flex justify-center">
+            <v-col cols="11" class="px-0 mb-3">
+              <DetailCard
+                :details="courseDetail"
+                :comments="commentText"
+                v-if="!status.commentLoading && !status.detailLoading"
+              />
+              <DetailCardLoader v-else />
+            </v-col>
+            <v-col cols="11">
+              <v-row>
+                <v-col lg="3" cols="12" class="pt-0 pl-0 pr-lg-4 pr-0">
+                  <FilterBox
+                    :style="adoptiveCardPosition"
+                    v-if="!status.commentLoading && !status.detailLoading"
+                  />
+                  <FilterBoxLoader v-else />
+                </v-col>
+                <v-col lg="9" cols="12" class="pl-lg-5">
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      class="px-0 pt-0 pb-3"
+                      v-if="
+                        !status.commentLoading &&
+                        !status.detailLoading &&
+                        global.isLogin
+                      "
                     >
-                    </CourseCommentCard>
-                  </v-col>
-                  <v-col
-                    class="pr-0 pl-0 pr-0 py-0"
-                    v-for="index in 2"
-                    :key="index"
-                    cols="12"
-                  >
-                    <CommentLoader
-                      height="290px"
-                      :max-width="null"
-                      v-if="status.commentLoading || status.detailLoading"
-                    />
-                  </v-col>
-                  <v-col
-                    v-if="
-                      selectedComment.length === 0 &&
-                      !status.commentLoading &&
-                      !status.detailLoading
-                    "
-                    class="d-flex justify-center"
-                    cols="12"
-                  >
-                    <nothing class="pt-10" :text="'还没有评论...'"></nothing>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
+                      <WritingCard />
+                    </v-col>
+                    <v-col
+                      class="pr-0 pl-0 pr-0 py-0"
+                      v-for="(comment, index) in selectedComment"
+                      :key="comment.id"
+                      cols="12"
+                    >
+                      <CourseCommentCard
+                        :comment="comment"
+                        v-if="!status.commentLoading && !status.detailLoading"
+                      >
+                      </CourseCommentCard>
+                    </v-col>
+                    <v-col
+                      class="pr-0 pl-0 pr-0 py-0"
+                      v-for="index in 2"
+                      :key="index"
+                      cols="12"
+                    >
+                      <CommentLoader
+                        height="290px"
+                        :max-width="null"
+                        v-if="status.commentLoading || status.detailLoading"
+                      />
+                    </v-col>
+                    <v-col
+                      v-if="
+                        selectedComment.length === 0 &&
+                        !status.commentLoading &&
+                        !status.detailLoading
+                      "
+                      class="d-flex justify-center"
+                      cols="12"
+                    >
+                      <nothing class="pt-10" :text="'还没有评论...'"></nothing>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
     </v-row>
   </v-container>
@@ -173,8 +175,4 @@ export default {
   },
 };
 </script>
-<style>
-body {
-  overflow-y: hidden;
-}
-</style>
+<style></style>
