b42197800024b06ac61ec83cc69e1c47dbb650ae
Tropinone <mspt5871@protonmail.com>
2023-03-02 16:07:06 +0800

    Improve pagination. Add link of CoursePrettier

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,85 +1,85 @@
 <template>
-  <div class='pt-sm-13 pt-6 d-flex justify-center'>
+  <div class="pt-sm-13 pt-6 d-flex justify-center">
     <div
       :style="{ width: $vuetify.breakpoint.width >= 1280 ? '1280px' : '100%' }"
-      class='about-container'
+      class="about-container"
     >
-      <div class='d-flex justify-center'>
-        <div class='text-sm-h1 text-h2 font-weight-bold mx-sm-0 mx-4'>
+      <div class="d-flex justify-center">
+        <div class="text-sm-h1 text-h2 font-weight-bold mx-sm-0 mx-4">
           To be the best bench.
         </div>
       </div>
-      <div class='d-flex justify-center pt-8 mb-16 mx-sm-0 mx-4'>
-        <div class='text-h4'>一个真实可靠、不断完善的多维课程评价信息库</div>
+      <div class="d-flex justify-center pt-8 mb-16 mx-sm-0 mx-4">
+        <div class="text-h4">一个真实可靠、不断完善的多维课程评价信息库</div>
       </div>
-      <div class='d-flex justify-center' style='flex-wrap: wrap'>
+      <div class="d-flex justify-center" style="flex-wrap: wrap">
         <div
-          class='mx-sm-6'
-          style='width: 168px'
+          class="mx-sm-6"
+          style="width: 168px"
         >
-          <div class='text-h6 d-flex justify-center'>
+          <div class="text-h6 d-flex justify-center">
             <div>真实评价</div>
           </div>
-          <div class='text-body-2 d-flex justify-center'>
+          <div class="text-body-2 d-flex justify-center">
             <div>Real Comments</div>
           </div>
         </div>
         <div
-          class='mx-sm-6'
-          style='width: 168px'
+          class="mx-sm-6"
+          style="width: 168px"
         >
-          <div class='text-h6 d-flex justify-center'>
+          <div class="text-h6 d-flex justify-center">
             <div>畅所欲言</div>
           </div>
-          <div class='text-body-2 d-flex justify-center'>
+          <div class="text-body-2 d-flex justify-center">
             <div>Free Discussion</div>
           </div>
         </div>
         <div
-          class='mx-sm-6'
-          style='width: 168px'
+          class="mx-sm-6"
+          style="width: 168px"
         >
-          <div class='text-h6 d-flex justify-center'>
+          <div class="text-h6 d-flex justify-center">
             <div>多维评分</div>
           </div>
-          <div class='text-body-2 d-flex justify-center'>
+          <div class="text-body-2 d-flex justify-center">
             <div>Comprehensive Grading</div>
           </div>
         </div>
         <div
-          class='mx-sm-6'
-          style='width: 168px'
+          class="mx-sm-6"
+          style="width: 168px"
         >
-          <div class='text-h6 d-flex justify-center'>
+          <div class="text-h6 d-flex justify-center">
             <div>简明易用</div>
           </div>
-          <div class='text-body-2 d-flex justify-center'>
+          <div class="text-body-2 d-flex justify-center">
             <div>User Friendly</div>
           </div>
         </div>
       </div>
-      <div class='d-flex justify-center mt-10'>
-        <v-sheet width='318px' class='pa-6 d-flex justify-center' outlined>
+      <div class="d-flex justify-center mt-10">
+        <v-sheet width="318px" class="pa-6 d-flex justify-center" outlined>
           <div>其他项目</div>
-          <div @click='openInplace("https://courseprettier.hcps.tech/")' class='coursePrettier'>CoursePrettier</div>
+          <div @click='openInplace("https://courseprettier.hcps.tech/")' class="coursePrettier">CoursePrettier</div>
         </v-sheet>
-        <v-sheet width='318px' class='pa-6 d-flex justify-center' outlined>
+        <v-sheet width="318px" class="pa-6 d-flex justify-center" outlined>
           <div>联系我们？</div>
           <div>
             <v-icon
-              class='ml-3'
+              class="ml-3"
               @click="openInplace('https://jq.qq.com/?_wv=1027&k=nJPBtvhe')"
             >{{ statics.icons.mdiQqchat }}
             </v-icon
             >
             <v-icon
-              class='ml-3'
+              class="ml-3"
               @click="openInplace('mailto:geekpie@geekpie.club')"
             >{{ statics.icons.mdiEmail }}
             </v-icon
             >
             <v-icon
-              class='ml-3'
+              class="ml-3"
               @click="
                 openInplace(
                   'https://github.com/ShanghaitechGeekPie/coursebench_official'
@@ -89,7 +89,7 @@
             </v-icon
             >
             <v-icon
-              class='ml-3'
+              class="ml-3"
               @click="openInplace('https://geekpie.club')"
             >{{ statics.icons.mdiWeb }}
             </v-icon
@@ -97,23 +97,23 @@
           </div>
         </v-sheet>
       </div>
-      <div class='mt-16'>
+      <div class="mt-16">
         <v-sheet
-          width='100%'
-          height='200px'
-          class='d-flex justify-center'
+          width="100%"
+          height="200px"
+          class="d-flex justify-center"
         >
-          <div class='d-flex justify-center flex-column'>
-            <div class='d-flex mx-2'>
-              <div class='d-flex justify-center flex-column'>
-                <div class='text-sm-h3 text-h4'>Powered by</div>
+          <div class="d-flex justify-center flex-column">
+            <div class="d-flex mx-2">
+              <div class="d-flex justify-center flex-column">
+                <div class="text-sm-h3 text-h4">Powered by</div>
               </div>
-              <v-img src='@/assets/geekpie.svg' :width="$vuetify.breakpoint.xsOnly ? '160px' : ''"></v-img>
+              <v-img src="@/assets/geekpie.svg" :width="$vuetify.breakpoint.xsOnly ? '160px' : ''"></v-img>
             </div>
           </div>
         </v-sheet>
       </div>
-      <div class='mt-16'>
+      <div class="mt-16">
         <div
           :class="[
             $vuetify.breakpoint.xsOnly
@@ -121,12 +121,12 @@
               : 'about-context',
           ]"
         >
-          <div class='py-8'>
-            <div class='text-h5'>开发人员</div>
-            <div class='mt-4 d-sm-flex d-block' style='flex-wrap: wrap'>
-              <div class='d-none'>
+          <div class="py-8">
+            <div class="text-h5">开发人员</div>
+            <div class="mt-4 d-sm-flex d-block" style="flex-wrap: wrap">
+              <div class="d-none">
                 <!--With great respect to our greatest developer-->
-                <div class='pr-4 pb-sm-4 pb-4 d-flex'>
+                <div class="pr-4 pb-sm-4 pb-4 d-flex">
                   <AvatarContainer
                     :src="
                       parseAvatarLink(
@@ -134,7 +134,7 @@
                       )
                     "
                     :name="'spedoske'"
-                    size='32'
+                    size="32"
                   />
                   <a
                     :href="parseHomeLink('https://github.com/Spedoske')"
@@ -144,22 +144,22 @@
                         : 'inline-text-black',
                       'inline-link-hover ml-2 mt-1',
                     ]"
-                  >{{ 'Spedoske' }}</a
+                  >{{ "Spedoske" }}</a
                   >
                 </div>
               </div>
               <div
-                v-for='(contributor, index) in contributors'
-                :key='index'
-                class='pr-4 pb-sm-4 pb-4 d-flex'
+                v-for="(contributor, index) in contributors"
+                :key="index"
+                class="pr-4 pb-sm-4 pb-4 d-flex"
               >
                 <AvatarContainer
-                  :src='parseAvatarLink(contributor.avatar)'
-                  :name='contributor.name'
-                  size='32'
+                  :src="parseAvatarLink(contributor.avatar)"
+                  :name="contributor.name"
+                  size="32"
                 />
                 <a
-                  :href='parseHomeLink(contributor.home)'
+                  :href="parseHomeLink(contributor.home)"
                   :class="[
                     $vuetify.theme.dark
                       ? 'inline-text-white'
@@ -172,37 +172,37 @@
             </div>
           </div>
           <v-divider></v-divider>
-          <div class='py-8' id='bug_report'>
-            <div class='text-h5'>错误反馈</div>
-            <div class='mt-4'>
+          <div class="py-8" id="bug_report">
+            <div class="text-h5">错误反馈</div>
+            <div class="mt-4">
               如果您在使用中遇到了问题，欢迎联系 <a
-              href='mailto:zhaoqch1@shanghaitech.edu.cn'
+              href="mailto:zhaoqch1@shanghaitech.edu.cn"
             >zhaoqch1@shanghaitech.edu.cn</a
             >
               或在 <a
-              href='https://github.com/ShanghaitechGeekPie/coursebench_official'
+              href="https://github.com/ShanghaitechGeekPie/coursebench_official"
             >GitHub Official Repository</a
             > 中提出Issue。
             </div>
           </div>
           <v-divider></v-divider>
-          <div class='py-8'>
-            <div class='text-h5'>友情赞助</div>
-            <div class='mt-4'>
+          <div class="py-8">
+            <div class="text-h5">友情赞助</div>
+            <div class="mt-4">
               <div
-                v-for='(sponsor, index) in sponsors.cooperations'
-                :key='index'
+                v-for="(sponsor, index) in sponsors.cooperations"
+                :key="index"
                 :class="['d-flex', 'justify-center', index === 0 ? '' : 'mt-8']"
               >
                 <div>
-                  <div class='d-flex justify-center' v-if="sponsor.logo !== ''">
-                    <div style='width: 56%'>
-                      <v-img :src='sponsor.logo'></v-img>
+                  <div class="d-flex justify-center" v-if="sponsor.logo !== ''">
+                    <div style="width: 56%">
+                      <v-img :src="sponsor.logo"></v-img>
                     </div>
                   </div>
-                  <div class='d-flex justify-center mt-2'>
+                  <div class="d-flex justify-center mt-2">
                     <a
-                      :href='sponsor.home'
+                      :href="sponsor.home"
                       :class="[
                         $vuetify.theme.dark
                           ? 'inline-text-white'
@@ -214,23 +214,23 @@
                   </div>
                 </div>
               </div>
-              <div class='pt-8 d-flex justify-center'>
+              <div class="pt-8 d-flex justify-center">
                 <div
-                  class='d-flex justify-center'
-                  style='flex-wrap: wrap; width: 340px'
+                  class="d-flex justify-center"
+                  style="flex-wrap: wrap; width: 340px"
                 >
                   <div
-                    v-for='(sponsor, index) in sponsors.individuals'
-                    :key='index'
-                    class='pr-4 pb-sm-4 pb-4 d-flex'
+                    v-for="(sponsor, index) in sponsors.individuals"
+                    :key="index"
+                    class="pr-4 pb-sm-4 pb-4 d-flex"
                   >
                     <AvatarContainer
-                      :src='parseAvatarLink(sponsor.avatar)'
-                      :name='sponsor.name'
-                      size='32'
+                      :src="parseAvatarLink(sponsor.avatar)"
+                      :name="sponsor.name"
+                      size="32"
                     />
                     <a
-                      :href='parseHomeLink(sponsor.home)'
+                      :href="parseHomeLink(sponsor.home)"
                       :class="[
                         $vuetify.theme.dark
                           ? 'inline-text-white'
@@ -245,31 +245,31 @@
             </div>
           </div>
           <v-divider></v-divider>
-          <div class='py-8'>
-            <div class='text-h5'>支持我们</div>
-            <div class='mt-4'>
+          <div class="py-8">
+            <div class="text-h5">支持我们</div>
+            <div class="mt-4">
               如果您觉得我们的网站对您有帮助，您可以通过点击以下链接找到CourseBench的专属赞助方案并支持我们：
-              <a href='https://afdian.net/a/colldisSavior'
+              <a href="https://afdian.net/a/colldisSavior"
               >爱发电（https://afdian.net/a/colldisSavior）</a
               >
             </div>
           </div>
           <v-divider></v-divider>
-          <div class='pt-8' id='service_terms'>
-            <div class='text-h5'>用户协议</div>
+          <div class="pt-8" id="service_terms">
+            <div class="text-h5">用户协议</div>
             <div
-              class='mt-4'
-              v-html='useMarkdown(termsOfService)'
-              style='white-space: pre-line'
+              class="mt-4"
+              v-html="useMarkdown(termsOfService)"
+              style="white-space: pre-line"
             ></div>
           </div>
           <v-divider></v-divider>
-          <div class='pt-8' id='privacy_policy'>
-            <div class='text-h5'>隐私政策</div>
+          <div class="pt-8" id="privacy_policy">
+            <div class="text-h5">隐私政策</div>
             <div
-              class='mt-4'
-              v-html='useMarkdown(privacyPolicy)'
-              style='white-space: pre-line'
+              class="mt-4"
+              v-html="useMarkdown(privacyPolicy)"
+              style="white-space: pre-line"
             ></div>
           </div>
         </div>
@@ -278,17 +278,17 @@
   </div>
 </template>
 <script>
-import AvatarContainer from '@/components/users/profile/AvatarContainer';
-import contributors from '@/assets/contributors.json';
-import sponsors from '@/assets/sponsors.json';
-import { mdiQqchat, mdiEmail, mdiGithub, mdiWeb } from '@mdi/js';
+import AvatarContainer from "@/components/users/profile/AvatarContainer";
+import contributors from "@/assets/contributors.json";
+import sponsors from "@/assets/sponsors.json";
+import { mdiQqchat, mdiEmail, mdiGithub, mdiWeb } from "@mdi/js";
 import {
   parseAvatarLink,
   parseHomeLink,
-} from '@/composables/global/useSponsors';
-import { openInplace } from '@/composables/global/useExternalUrl';
-import useMarkdown from '@/composables/global/useMarkdown';
-import { termsOfService, privacyPolicy } from 'Policy';
+} from "@/composables/global/useSponsors";
+import { openInplace } from "@/composables/global/useExternalUrl";
+import useMarkdown from "@/composables/global/useMarkdown";
+import { termsOfService, privacyPolicy } from "Policy";
 
 export default {
   setup() {
