caa80f841eedb0b47534eb31f6e04b423f79ad51
longxiaokong <david060708@126.com>
2024-06-29 16:30:21 +0800

    local added usemenubar

---
+++ src/composables/global/useMenuBar.js
@@ -0,0 +1,21 @@
+import { inject, onMounted, onUnmounted } from 'vue';
+
+export default () => {
+  const menuBarStatus = inject('menu');
+
+  onMounted(() => {
+    updateMenuStatus();
+    window.addEventListener('resize', updateMenuStatus, { passive: true });
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', updateMenuStatus, { passive: true });
+  });
+
+  const updateMenuStatus = () => {
+    menuBarStatus.drawer =
+      window.innerWidth >= 960 ? false : menuBarStatus.drawer;
+  };
+
+  return { menuBarStatus };
+};
