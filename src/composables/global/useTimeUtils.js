7ead8a942865a2e4e1e62389f0b742510ddcb2e5
ZAMBAR <zambar@shanghaitech.edu.cn>
2026-02-27 04:12:10 +0800

    Dev/reply (#51)
    * feat: reply support
    
    * fix: annonymuous bug
    
    * feat: reply orders
    
    * feat: scroll to comment
    
    * temp: disabled share reply
    
    * chore: ui fix
    
    * fix: disabled hash
    
    * chore: time fix
    
    * fix: padding of teachers

--- src/composables/global/useTimeUtils.js
+++ src/composables/global/useTimeUtils.js
@@ -1,5 +1,11 @@
 function unixToReadable(unix) {
-  return new Date(unix * 1000).toISOString().slice(0, 19).replace('T', ' ');
+  return new Date(unix * 1000).toLocaleString('zh-CN', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 }
 
 export { unixToReadable };
